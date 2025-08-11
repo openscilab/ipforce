dfa3f6bfca6c23ba9e629d40006aa3abb2fecabb
AHReccese <amirhssin6rst@gmail.com>
2025-08-11 01:58:10 -0400

    expose both adapters

--- ipforce/__init__.py
+++ ipforce/__init__.py
@@ -1,5 +1,5 @@
 """ipforce modules."""
 from .params import IPFORCE_VERSION
-from .adapters import IPv4HTTPAdapter
+from .adapters import IPv4HTTPAdapter, IPv6HTTPAdapter
 
 __version__ = IPFORCE_VERSION
