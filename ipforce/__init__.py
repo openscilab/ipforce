fdbfb0add4e3fc513959838a36fd8a7079f0d1af
Amirhossein Rostami <amirhssin6rst@gmail.com>
2026-06-01 16:57:07 -0400

    Add :: `Lock` based thread-safe adapter (#20)
    * Add thread-safe (`Lock` based) HTTP adapters for IPv4 and IPv6 support
    
    * add Lock adapters
    
    * add Lock adapters
    
    * add comprehensive test suite for Lock-based adapter
    
    * update test command
    
    * `CHANGELOG.md` updated
    
    * `README.md` updated
    
    * add docstring
    
    Added parameter documentation to filtered_getaddrinfo function.

--- ipforce/__init__.py
+++ ipforce/__init__.py
@@ -2,7 +2,11 @@
 """ipforce modules."""
 from .params import IPFORCE_VERSION
 from .adapters import IPv4TransportAdapter, IPv6TransportAdapter
+from .adapters import IPv4LockAdapter, IPv6LockAdapter
 
 __version__ = IPFORCE_VERSION
 
-__all__ = ["IPv4TransportAdapter", "IPv6TransportAdapter"]
+__all__ = [
+    "IPv4TransportAdapter", "IPv6TransportAdapter",
+    "IPv4LockAdapter", "IPv6LockAdapter",
+]
