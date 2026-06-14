4e3775eb86b68936f04df749760241fde6a3cd44
AHReccese <amirhssin6rst@gmail.com>
2026-06-14 19:16:14 -0400

    expose factory classes

--- ipforce/__init__.py
+++ ipforce/__init__.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 """ipforce modules."""
 from .params import IPFORCE_VERSION
+from .enums import IPVersion, IPForceMethod
+from .api import IPForceAdapter, IPForceSession
 from .adapters import IPv4TransportAdapter, IPv6TransportAdapter
 from .adapters import IPv4LockAdapter, IPv6LockAdapter
 
 __version__ = IPFORCE_VERSION
 
 __all__ = [
+    "IPVersion", "IPForceMethod",
+    "IPForceAdapter", "IPForceSession",
     "IPv4TransportAdapter", "IPv6TransportAdapter",
     "IPv4LockAdapter", "IPv6LockAdapter",
 ]
